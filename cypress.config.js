55e53894629a28d944cea749245cb6c75ade93bd
Peter Zosiak <peter.zosiak@outlook.com>
2025-06-03 11:16:49 +0200

    Refactor Cypress workflow to streamline input handling and enhance configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
+  projectId: "1i42de",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
